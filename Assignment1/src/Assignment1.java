1e62d276b2ff49716fa296cab8753aff0f8b0b21
didikid3 <34197234+didikid3@users.noreply.github.com>
2020-09-02 12:10:43 -0700

    Initial Push

---
+++ Assignment1/src/Assignment1.java
@@ -0,0 +1,14 @@
+/*
+// AUTHOR: Brandon Chao
+// FILENAME: Assignment1.java
+// SPECIFICATION: Pt. 1 Descriptions to Expressions.java     Pt. 2 Program to display distances given by user in different units 
+// FOR: CS 1400 - ASSIGNMENT #1
+// TIME SPENT: 
+//*/
+
+import java.util.Scanner;
+
+public class Assignment1 
+{
+	Scanner scanner=new Scanner(System.in);
+}
